334e28e003d176d73a5b259cb6c1e6f114de585e
suresh <pratiksha.indalkar@futurescapetech.com>
2023-11-07 17:40:16 +0530

    Tested basic query operations related to products and product categories

--- examples/drift-example/lib/src/schemas/schemas.dart
+++ examples/drift-example/lib/src/schemas/schemas.dart
@@ -3,24 +3,14 @@ import 'dart:io';
 
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
+import 'package:drift_example/src/tables/product_categories.dart';
+import 'package:drift_example/src/tables/products.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
 part 'schemas.g.dart';
 
-class ProductTable extends Table {
-
-  IntColumn get id => integer().autoIncrement()();
-  TextColumn get prodId => text().nullable()();
-  TextColumn get name => text()();
-  RealColumn get price => real().withDefault(const Constant(0))();
-  TextColumn get sku => text().nullable()();
-  TextColumn get shortCode => text().nullable()();
-  TextColumn get image => text().nullable()();
-  BoolColumn get status => boolean().withDefault(const Constant(false))();
-}
-
-@DriftDatabase(tables: [ProductTable])
+@DriftDatabase(tables: [Products, ProductCategories])
 class AppDatabase extends _$AppDatabase {
 
   AppDatabase() : super(_openConnection());
